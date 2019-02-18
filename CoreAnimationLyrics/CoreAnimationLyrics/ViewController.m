833e224c6d9a1718b05e3cf022dd0058d9a70b1c
annieli <annieli@kkbox.com>
2019-02-18 16:04:48 +0800

    Add lyrics view and moveUp animation.

--- CoreAnimationLyrics/CoreAnimationLyrics/ViewController.m
+++ CoreAnimationLyrics/CoreAnimationLyrics/ViewController.m
@@ -9,14 +9,130 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property UIView *lyricsView;
+@property NSInteger current;
+@property NSTimer *timer;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    
+    self.current = 0;
+    
+    [self.view setBackgroundColor:[UIColor darkGrayColor]];
+    
+    NSArray *lyrics = [self parseFile:@"Lyrics" type:@"txt"];
+    
+    [self setupLyricsView];
+    
+    [self addLyricsTextLayer:lyrics];    
 }
 
+- (NSArray*)parseFile:(NSString*)file type:(NSString*)type
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:file ofType:type];
+    
+    NSString *parseJson = [[NSString alloc] initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    
+    NSArray *lyrics = [parseJson componentsSeparatedByString:@"\n"];
+    
+    return lyrics;
+}
+
+- (void)setupLyricsView
+{
+    self.lyricsView = [UIView new];
+    self.lyricsView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.lyricsView.clipsToBounds = YES;
+    self.lyricsView.backgroundColor = [UIColor darkGrayColor];
+    
+    [self.view addSubview:self.lyricsView];
+    
+    NSLayoutConstraint *widthConstraint = [NSLayoutConstraint constraintWithItem:self.lyricsView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:[[UIScreen mainScreen] bounds].size.width];
+    [self.view addConstraint:widthConstraint];
+    
+    NSLayoutConstraint *heightConstraint = [NSLayoutConstraint constraintWithItem:self.lyricsView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:500];
+    [self.view addConstraint:heightConstraint];
+    
+    NSLayoutConstraint *verticalConstraint = [NSLayoutConstraint constraintWithItem:self.lyricsView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0];
+    [self.view addConstraint:verticalConstraint];
+    
+    NSLayoutConstraint *horizontalConstraint = [NSLayoutConstraint constraintWithItem:self.lyricsView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0];
+    [self.view addConstraint:horizontalConstraint];
+}
+
+- (void)addLyricsTextLayer:(NSArray*)lyrics
+{
+    for (int i = 0; i < lyrics.count; i++) {
+        
+        CATextLayer *textLayer = [CATextLayer new];
+        textLayer.frame = CGRectMake(0, i * 30, [[UIScreen mainScreen] bounds].size.width, 30);
+        
+        [textLayer setFont:@"Helvetica"];
+        [textLayer setFontSize:20];
+        [textLayer setString:lyrics[i]];
+        [textLayer setAlignmentMode:kCAAlignmentCenter];
+        [textLayer setForegroundColor:[[UIColor whiteColor] CGColor]];
+        [textLayer setBackgroundColor:[[UIColor darkGrayColor] CGColor]];
+
+        [self.lyricsView.layer addSublayer:textLayer];
+    }
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer:) userInfo:nil repeats:YES];
+}
+
+- (void)updateLayers
+{
+    NSArray<CATextLayer *> *textLayers = self.lyricsView.layer.sublayers;
+    
+    for (int i = 0; i < textLayers.count; i++) {
+        
+        if (i == self.current) {
+
+            [textLayers[i] setForegroundColor:[[UIColor blackColor] CGColor]];
+            [textLayers[i] setBackgroundColor:[[UIColor lightGrayColor] CGColor]];
+            
+            if (i != 0) {
+                [textLayers[i-1] setForegroundColor:[[UIColor whiteColor] CGColor]];
+                [textLayers[i-1] setBackgroundColor:[[UIColor darkGrayColor] CGColor]];
+            }
+        }
+        
+        if (self.current >= 9) {
+            [self moveUpTextLayer:textLayers[i]];
+        }
+    }
+}
+
+- (void)moveUpTextLayer:(CATextLayer*)textlayer
+{
+    CGPoint originPosition = CGPointMake(textlayer.position.x, textlayer.position.y);
+    CGPoint newPosition = CGPointMake(originPosition.x, originPosition.y - 30);
+
+    CABasicAnimation *moveUp = [CABasicAnimation animationWithKeyPath:@"position"];
+    moveUp.fromValue = [NSValue valueWithCGPoint:originPosition];
+    moveUp.toValue = [NSValue valueWithCGPoint:newPosition];
+    moveUp.duration   = 1.0;
+    [textlayer addAnimation:moveUp forKey:moveUp.keyPath];
+    
+    textlayer.position = newPosition;
+}
+
+- (void)updateTimer:(NSTimer *)timer
+{
+    [self updateLayers];
+    
+    self.current++;
+    
+    if (self.current == self.lyricsView.layer.sublayers.count) {
+        
+        [self.timer invalidate];
+        
+        self.timer = nil;
+    }
+}
 
 @end
